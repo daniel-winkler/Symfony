d208f6f8bb09063996f61ffcfa88ca3ff9caecc7
dwinkler <danielbwo@gmail.com>
2021-06-14 14:03:19 +0200

    vemos como enlazamos distintas rutas en nuestro html de distintas maneras

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,6 +6,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,7 +30,19 @@ class DefaultController extends AbstractController{
     // El primer parámetro de Route es la URL a la que queremos asociar la acción.
     // El segundo parámetro de Route es el nombre que queremos dar a la ruta.
 
-    public function index(): Response {
+    public function index(Request $request): Response {
+
+        if($request->query->has('id')) {
+            echo '<pre>'; var_dump($request->get('id')); echo '</pre>'; 
+            die();
+        }
+
+        // echo '<pre>'; var_dump($request->query); echo '</pre>'; // equivalente a la superglobal $_GET de php
+        // echo '<pre>'; var_dump($request->request); echo '</pre>'; // equivalente a la superglobal $_POST de php
+        // echo '<pre>'; var_dump($request->server); echo '</pre>'; // equivalente a la superglobal $_SERVER de php
+        // echo '<pre>'; var_dump($request->files); echo '</pre>'; // equivalente a la superglobal $_FILES de php
+        // echo '<pre>'; var_dump($request->cookies); echo '</pre>'; // equivalente a la superglobal $_COOKIE de php
+        // die();
 
         // Una acción siempre debe devolver una respesta.
         // Por defecto deberá ser un objeto de la clase,
@@ -70,6 +84,44 @@ class DefaultController extends AbstractController{
     public function indexJson(): JsonResponse {
         return $this->json(self::PEOPLE); // con este metodo podriamos utilizar la clase Response, ya que la funcion json() utiliza JsonResponse.
         // return new JsonResponse(self::PEOPLE); // con self accedemos a constantes dentro de la clase
+    }
+
+
+    /**
+     * @Route(
+     *      "/default/{id}",
+     *      name="default_show",
+     *      requirements = {
+     *          "id": "[0-3]"
+     *      }
+     * )
+     */
+
+    public function show(int $id): Response {
+        // var_dump($id); die();
+        return $this->render('default/show.html.twig', [
+            'id' => $id,
+            'person' => self::PEOPLE[$id]
+        ]);
+    }
+
+    /**
+     * @Route(
+     *      "/redirect-to-home",
+     *      name="default_redirect_to_home"
+     * )
+     */
+    public function redirectToHome(): Response {
+        
+        // Redirigir a la URL "/"
+        // return $this->redirect('/');
+
+        // Redirigir a una ruta utilizando su nombre.
+        // return $this->redirectToRoute('default_show', [
+        //     'id' => 1
+        // ]);
+
+        return new RedirectResponse("/", Response::HTTP_TEMPORARY_REDIRECT);
     }
 }
 
