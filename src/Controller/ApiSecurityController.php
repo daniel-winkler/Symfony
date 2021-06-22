00d6ff0f3a48b30e7f9aaf6c396141758654b4e1
dwinkler <danielbwo@gmail.com>
2021-06-22 10:51:45 +0200

    añadimos token a nuestro login

--- src/Controller/ApiSecurityController.php
+++ src/Controller/ApiSecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,13 +12,27 @@ class ApiSecurityController extends AbstractController
     /**
      * @Route("/api/login", name="api_security", methods={"POST"})
      */
-    public function login(): Response
+    public function login(
+        EntityManagerInterface $entityManagerInterface
+    ): Response
     {
         $user = $this->getUser();
 
+        $token = sha1(random_bytes(32)); // sha1 normaliza los bytes generados para que no salgan caracteres raros
+        $now = new \DateTime();
+        $tokenExpiration = $now->add(new \DateInterval('PT1H'));
+
+        $user->setToken($token);
+        $user->setTokenExpiration($tokenExpiration);
+
+        // $entityManagerInterface->persist();
+        $entityManagerInterface->flush();
+
         return $this->json([
             'username' => $user->getUserIdentifier(),
-            'roles' => $user->getRoles()
+            'roles' => $user->getRoles(),
+            'token' => $user->getToken(),
+            'token_expiration' => $user->getTokenExpiration()
         ]);
     }
 }
